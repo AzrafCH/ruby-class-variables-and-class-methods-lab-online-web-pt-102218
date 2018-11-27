7c89548759591ec2457503bd55c96d1ee3f67226
Azraf Chowdhury <achowdhur@ltu.edu>
2018-11-27 21:35:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,18 @@
 class Song 
   
-  attr_accessor :name, :artists, :genre 
+  attr_accessor :name, :artist, :genre 
   
   @@count = 0
   
   def initialize(name,artists,genre)
     @@count += 1 
     @name = name 
-    @artists = artists 
+    @artist = artist 
     @genre = genre 
     @@genres = [] 
+    @@genres << genre
+    @@artists = [] 
+    @@artists << artist
   end 
   
   def self.count 
@@ -17,7 +20,7 @@ class Song
   end 
   
   def self.genres
-    
+    @@genres.uniq 
   end 
   
 end 
